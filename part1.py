39f8f917cf8fee6b714d9ca386d4bdf2c2bc73c3
Bart Massey <bart.massey@gmail.com>
2023-12-03 22:23:31 -0800

    got initial solution for part 1; input parsing failed

--- part1.py
+++ part1.py
@@ -2,3 +2,25 @@
 # Code by Github Copilot Chat
 # Editing by Bart Massey
 
+import sys
+
+def calculate_score(winning_numbers, your_numbers):
+    matches = [num for num in your_numbers if num in winning_numbers]
+    if not matches:
+        return 0
+    else:
+        return 2 ** (len(matches) - 1)
+
+def main():
+    filename = sys.argv[1]
+    total_score = 0
+    with open(filename, 'r') as file:
+        for line in file:
+            winning_numbers, your_numbers = line.split('|')
+            winning_numbers = set(map(int, winning_numbers.split()))
+            your_numbers = list(map(int, your_numbers.split()))
+            total_score += calculate_score(winning_numbers, your_numbers)
+    print(total_score)
+
+if __name__ == "__main__":
+    main()
