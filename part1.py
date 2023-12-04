1fc332b4a507bd8428b2ec40436356eb56e88f12
Bart Massey <bart.massey@gmail.com>
2023-12-03 22:24:42 -0800

    completed part 1

--- part1.py
+++ part1.py
@@ -16,7 +16,8 @@ def main():
     total_score = 0
     with open(filename, 'r') as file:
         for line in file:
-            winning_numbers, your_numbers = line.split('|')
+            _, numbers = line.split(':')
+            winning_numbers, your_numbers = numbers.split('|')
             winning_numbers = set(map(int, winning_numbers.split()))
             your_numbers = list(map(int, your_numbers.split()))
             total_score += calculate_score(winning_numbers, your_numbers)
